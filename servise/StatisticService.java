e01bb9b92b3a96c08f448c6cb5d41cfe909b54e7
Sotiboldiyev Azizbek <sotiboldiyevazizbek86@gmail.com>
2023-03-07 18:12:41 +0500

    adding new menu statics

---
+++ servise/StatisticService.java
@@ -0,0 +1,12 @@
+package servise;
+
+import controller.TransactionController;
+import repository.TransactionRepository;
+
+public class StatisticService {
+
+    public void todayPayments() {
+//        TransactionRepository transactionRepository=new TransactionRepository();
+//        transactionRepository.
+    }
+}
